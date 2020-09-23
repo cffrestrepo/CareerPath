d724d2196f62a3f0c2c757958210f8ce4869f0a8
Cris <cff.restrepo@hotmail.com>
2020-09-23 16:39:23 -0500

    Creando las actividades y los fragmentos

---
+++ app/src/main/java/com/career/careerpath/ui/activities/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.career.careerpath.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.career.careerpath.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
