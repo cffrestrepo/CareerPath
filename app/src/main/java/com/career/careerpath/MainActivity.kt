1dc3ad95d788b707b46443c0f5a8b2c93b154594
Cris <cff.restrepo@hotmail.com>
2020-09-25 21:38:29 -0500

    Implementando la libreria de navegacion del navgraph

--- app/src/main/java/com/career/careerpath/MainActivity.kt
+++ app/src/main/java/com/career/careerpath/MainActivity.kt
@@ -2,10 +2,15 @@ package com.career.careerpath
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.career.careerpath.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
