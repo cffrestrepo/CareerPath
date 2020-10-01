a47df99b7d9f8e90cc600ae4d414152385c81518
Cris <cff.restrepo@hotmail.com>
2020-09-30 20:54:25 -0500

    Adelantando modelos arquitectura MVVM

---
+++ app/src/main/java/com/career/careerpath/model/Conference.kt
@@ -0,0 +1,10 @@
+package com.career.careerpath.model
+
+class Conference {
+
+    lateinit var title : String
+    lateinit var description : String
+    lateinit var tag : String
+    lateinit var datetime : String
+
+}
---
+++ app/src/main/java/com/career/careerpath/model/Ubication.kt
@@ -0,0 +1,13 @@
+package com.career.careerpath.model
+
+class Ubication {
+
+    var name = ""
+    var address = ""
+    var latitude = ""
+    var longitude = ""
+    var phone = ""
+    var website = ""
+    var photo = ""
+
+}
