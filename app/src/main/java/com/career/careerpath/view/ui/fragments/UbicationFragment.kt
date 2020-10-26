24e5ca47c9980bfd8c3c66529464e66a1e673057
Cris <cff.restrepo@hotmail.com>
2020-10-25 19:56:57 -0500

    Implementando el mapa dialogo y accion sobre el mapa

--- app/src/main/java/com/career/careerpath/view/ui/fragments/UbicationFragment.kt
+++ app/src/main/java/com/career/careerpath/view/ui/fragments/UbicationFragment.kt
@@ -1,17 +1,27 @@
 package com.career.careerpath.view.ui.fragments
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
 import com.career.careerpath.R
+import com.career.careerpath.model.Ubication
 import com.google.android.gms.dynamic.SupportFragmentWrapper
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 
-class UbicationFragment : Fragment(), OnMapReadyCallback {
+class UbicationFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
     private var param1: String? = null
     private var param2: String? = null
 
@@ -32,8 +42,31 @@ class UbicationFragment : Fragment(), OnMapReadyCallback {
         mapFragment.getMapAsync(this@UbicationFragment)
     }
 
-    override fun onMapReady(p0: GoogleMap?) {
-        
+    override fun onMapReady(googleMap: GoogleMap?) {
+        val ubication = Ubication()
+        val zoom = 16f
+        val centerMap = LatLng(ubication.latitude.toDouble(), ubication.longitude.toDouble())
+
+        googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(centerMap, zoom))
+
+        val centerMark = LatLng(ubication.latitude.toDouble(), ubication.longitude.toDouble())
+        val markeroptions = MarkerOptions()
+        markeroptions.position(centerMark)
+        markeroptions.title("Cris")
+        val bitmapDraw = context?.applicationContext?.let {
+            ContextCompat.getDrawable(it, R.drawable.ic_map_marker)
+        } as BitmapDrawable
+
+        val smallMarker = Bitmap.createScaledBitmap(bitmapDraw.bitmap, 150, 150, false)
+        markeroptions.icon(BitmapDescriptorFactory.fromBitmap(smallMarker))
+
+        googleMap?.addMarker(markeroptions)
+        googleMap?.setOnMarkerClickListener(this)
+    }
+
+    override fun onMarkerClick(p0: Marker?): Boolean {
+        findNavController().navigate(R.id.ubicationDetailDialogFragment)
+        return true
     }
 
 }
